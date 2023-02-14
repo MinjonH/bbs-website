50e42d7de430e2eef1671bc79cffa33a901bc369
MinjonH <97527244+MinjonH@users.noreply.github.com>
2023-02-14 20:28:56 +0200

    some changes

--- pages/projects/laminatedbeams.js
+++ pages/projects/laminatedbeams.js
@@ -13,9 +13,9 @@ const laminatedBeams = ({ images }) => {
 			</Head>
 			<div className='w-full'>
 				<div className='mx-12 px-2 py-20'>
-					<p className='font-black text-white text-5xl md:text-[40pt] font-ttHoves mt-4 mb-8'>
+					<h1 className='font-black text-white text-5xl md:text-[40pt] font-ttHoves mt-4 mb-8'>
 						Laminated Beam Projects
-					</p>
+					</h1>
 
 					<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8'>
 						{images.map(({ public_id, format }) => (
