d33bbf31bc0015f0b238b32cd693cd6a4af73635
MinjonH <97527244+MinjonH@users.noreply.github.com>
2023-02-14 20:13:38 +0200

    installed missing files

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-import Contact from '@/components/Contact';
+import Contact from '../components/Contact';
 
 function contact() {
 	return (
