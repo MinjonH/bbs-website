baaf8abea9e25d34a3a8e720be2494e43b4bc3a1
MinjonH <97527244+MinjonH@users.noreply.github.com>
2023-02-14 20:55:38 +0200

    commit

--- components/ModalButton.jsx
+++ components/ModalButton.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ModalButton = ({ buttonRef, showModal }) => {
 	return (
-		<button ref={buttonRef} onClick={showModal}>
+		<div ref={buttonRef} onClick={showModal}>
 			<li className='blueButton text-md font-semibold'>Get a quote</li>
-		</button>
+		</div>
 	);
 };
 export default ModalButton;
