12434142f3e99f0d37052fc1fea3a7094501415e
MinjonH <97527244+MinjonH@users.noreply.github.com>
2023-02-15 20:11:15 +0200

    cloudinary fixes

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -2,9 +2,9 @@ import cloudinary from 'cloudinary';
 
 // @ts-ignore
 cloudinary.config({
-	cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME || null,
-	api_key: process.env.CLOUDINARY_API_KEY || null,
-	api_secret: process.env.CLOUDINARY_API_SECRET || null,
+	cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
 	secure: true,
 });
 
