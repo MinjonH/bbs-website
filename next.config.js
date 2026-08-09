d527eaddcd2b993cc45ddb9ea93471aa47c74ad3
Minjon <97527244+MinjonH@users.noreply.github.com>
2026-08-09 14:05:31 +0200

    Add files via upload
    next convig js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	images: {
-		domains: ['res.cloudinary.com'],
-	},
+  reactStrictMode: true,
+  output: 'export',
+  images: {
+    domains: ['res.cloudinary.com'],
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
+
